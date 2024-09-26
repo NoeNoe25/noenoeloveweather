f375703eb4f9d199934ba6dc81e18b55d69ef08c
NoeNoe25 <144063126+NoeNoe25@users.noreply.github.com>
2024-09-27 01:45:58 +0700

    adding images to src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import Weather from "./search_engine";
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      Hello from App
+      <div class="container">
+        <div class="design"> 
+         
+         <Weather defaultCity="Mawlamyine"/>
+
+        <div class="row">
+            <div class="col-12">
+            <footer>
+            This project was coded by Noe Noe,
+            <a href="https://github.com/NoeNoe25/Refined_Weather_Project">
+              is open-sourced on Github
+            </a>
+            and{" "}
+            <a href="https://benevolent-tanuki-f00e59.netlify.app">
+              {" "}hosted on Netlify{" "}
+            </a>
+          </footer>
+            </div>
+
+          </div>
+   
+          
+        </div>
+      </div>
     </div>
   );
 }
