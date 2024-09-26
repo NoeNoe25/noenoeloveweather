894444e3023b4850d13b288ff2dd6ad48289ab79
NoeNoe25 <144063126+NoeNoe25@users.noreply.github.com>
2024-09-27 01:47:01 +0700

    creating search engine for search box and button

---
+++ src/search_engine.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import axios from "axios";
+import './style.css';
+import WeatherIcon from "./WeatherIcon";
+import WeatherForecast from "./WeatherForecast";
+
+export default function Weather() {
+  let [name, setName] = useState("");
+  let [temperature, setTemperature] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [icon, setIcon] = useState(""); 
+  let [date, setDate] = useState(null);
+  let [coord1, setCoord1] = useState(null);
+  let [coord2, setCoord2] = useState(null);
+
+
+  function showTemp(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(response.data.weather[0].icon);  // Set icon from API
+    setDate(new Date(response.data.dt* 1000));
+    setCoord1(response.data.coord.lat);
+    setCoord2(response.data.coord.lon);
+  
+  }
+  console.log(name);
+
+
+  function search(event) {
+   
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=88724523008dc9e1be18f6eb6a959b67&units=metric`;
+    axios.get(url).then(showTemp);
+  }
+
+  function updateUsername(event) {
+    setName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function formatDate(date) {
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hour}:${minute}`;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            className="form-control"
+            required
+            placeholder="Enter a city"
+            onChange={updateUsername}
+            value={name}
+          />
+        </div>
+        <div className="col-3 p-0">
+          <button className="btn btn-outline-secondary" type="submit">
+            Search
+          </button>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6" id="t_all">
+          <ul className="text">
+            <li id="city">{name}</li>
+            <li id="time">
+              {date && formatDate(date)}, {description}
+            </li>
+            <li>
+              Humidity: <strong id="humidity">{humidity}</strong>% , Wind:{" "}
+              <strong id="wind">{wind}</strong> km/h
+            </li>
+          </ul>
+        </div>
+        <div className="col-lg-6 col-md-6 col-sm-8">
+          <div className="temperature-container d-flex justify-content-end">
+            <span id="icon">
+              <WeatherIcon state={icon} width={100} height={100} />
+            </span>
+            <span id="degree">{temperature} </span>
+            {temperature && <span id="c">°C</span>}
+          </div>
+        </div>
+      </div>
+      <div className="row">
+ 
+      </div>
+      <WeatherForecast city1={coord1} city2={coord2}/>
+  
+
+   
+    </form>
+  );
+}
